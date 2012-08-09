80c92141c64cb68b70590ee33e69ad844385e678
Tim Bezhashvyly <tim@openstream.ch>
2012-08-09 10:08:42 +0200

    OME-1: Fixed top influencers links

--- app/code/community/Openstream/Osmm/Model/Searchinfluencers.php
+++ app/code/community/Openstream/Osmm/Model/Searchinfluencers.php
@@ -7,4 +7,12 @@ class Openstream_Osmm_Model_Searchinfluencers extends Mage_Core_Model_Abstract
 		parent::_construct();
         $this->_init('osmm/searchinfluencers');
     }
+
+    public function getUrl() {
+        if ($this->getData('search_source') == 'facebook') {
+            return 'http://www.facebook.com/'.$this->getData('search_author_name');
+        } else {
+            return 'https://twitter.com/'.$this->getData('search_author_name');
+        }
+    }
 }
